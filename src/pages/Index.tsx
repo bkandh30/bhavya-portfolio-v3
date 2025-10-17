2626c02aabfa253838026680837fdc4e562f00a5
bkandh30 <kandhari.bhavya20@gmail.com>
2025-10-16 19:27:02 -0700

    Created the index page

---
+++ src/pages/Index.tsx
@@ -0,0 +1,17 @@
+const Index = () => {
+  return (
+    <div className="min-h-screen bg-navy flex items-center justify-center">
+      <div className="text-center">
+        <h1 className="text-5xl font-bold text-slate mb-4">Main Index Page</h1>
+        <p className="text-xl text-slate-muted">
+          Main Portfolio Page Content Lorem ipsum dolor sit amet consectetur
+          adipisicing elit. Qui ab fuga molestiae explicabo quaerat saepe
+          eligendi deserunt, voluptatum laboriosam ex esse adipisci
+          exercitationem est maxime minima ipsam, vitae, reiciendis odio?
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Index;

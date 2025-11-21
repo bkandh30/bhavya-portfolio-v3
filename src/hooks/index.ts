c5e39d58db11ac0c4452814e16d4c2d328d26e2e
bkandh30 <kandhari.bhavya20@gmail.com>
2025-11-20 17:43:05 -0700

    Created hook index

---
+++ src/hooks/index.ts
@@ -0,0 +1,6 @@
+export { useActiveSection } from './useActiveSection';
+export { useFadeInObserver } from './useFadeInObserver';
+export { useMousePosition } from './useMousePosition';
+export { useScrollToSection } from './useScrollToSection';
+export { useSmoothScroll } from './useSmoothScroll';
+export { useEmailCopy } from './useEmailCopy';

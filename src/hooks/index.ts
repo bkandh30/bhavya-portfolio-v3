7825ac8c6583d7785bf7d3db145ba8cf093bd469
bkandh30 <kandhari.bhavya20@gmail.com>
2026-01-26 18:24:17 -0700

    Removed unused hooks

--- src/hooks/index.ts
+++
@@ -1,8 +0,0 @@
-export { useActiveSection } from './useActiveSection';
-export { useFadeInObserver } from './useFadeInObserver';
-export { useMousePosition } from './useMousePosition';
-export { useScrollToSection } from './useScrollToSection';
-export { useSmoothScroll } from './useSmoothScroll';
-export { useEmailCopy } from './useEmailCopy';
-export { useKeyboardNavigation } from './useKeyboardNavigation';
-export { usePerformanceMonitor } from './usePerformanceMonitor';

2a276bca82d0a30b54cd6e66b5a52039aa018d60
bkandh30 <kandhari.bhavya20@gmail.com>
2026-02-03 17:28:23 -0700

    Integrated Vercel speed insights

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,12 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
+import { injectSpeedInsights } from "@vercel/speed-insights";
 import { applySystemThemeClass } from "@/lib/theme";
+
+if (import.meta.env.PROD) {
+  injectSpeedInsights();
+}
 
 applySystemThemeClass();
 
--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -16,7 +16,6 @@ import { useFadeInObserver } from "@/hooks/useFadeInObserver";
 import { useScrollToSection } from "@/hooks/useScrollToSection";
 import { useSmoothScroll } from "@/hooks/useSmoothScroll";
 import { useKeyboardNavigation } from "@/hooks/useKeyboardNavigation";
-import { usePerformanceMonitor } from "@/hooks/usePerformanceMonitor";
 
 const SECTION_IDS = [
   "about",
@@ -46,7 +45,6 @@ const Index = () => {
   useFadeInObserver();
   useKeyboardNavigation(SECTION_IDS, activeSection, scrollToSection);
   useMousePosition(true);
-  usePerformanceMonitor();
 
   return (
     <div
